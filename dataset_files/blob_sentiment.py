bf113ded5ffcbeb53a5cbf17c5913e3c87ddb909
gitEmme <donadimartina@gmail.com>
2018-07-04 11:06:53 +0200

    RNN but bad accuracy

--- dataset_files/blob_sentiment.py
+++ dataset_files/blob_sentiment.py
@@ -1,19 +1,15 @@
-import textblob_de
 from textblob_de import TextBlobDE
 from textblob_de.classifiers import NaiveBayesClassifier
 import pickle
 
-from textblob_de import TextBlobDE
-from textblob_de.classifiers import NaiveBayesClassifier
-import pickle
-
-with open('rawtext_labeled', 'rb') as fs:
+with open('raw_num_labeled', 'rb') as fs:
     training_list = pickle.load(fs)
-cl=NaiveBayesClassifier(training_list[:500])
-cl.classify('das ist echt toll')
+cl=NaiveBayesClassifier(training_list[:3000])
+#cl.classify('das ist echt toll')
+print(cl.classify('das ist echt toll'))
 blob=TextBlobDE("Das ist super schade. Das tut mir so leid.")
 
 for s in blob.sentences:
     print(s)
     print(cl.classify(s))
-print(cl.accuracy(training_list[:1000]))
+print(cl.accuracy(training_list[3000:]))
